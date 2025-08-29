ceffea1b752a9436f3ec1cb925275bf284042000
StepanHovsepyan7 <stpnhovsepyan@gmail.com>
2025-08-30 01:53:15 +0400

    created race track for cars

---
+++ src/App/Features/Garage/components/Race-track/Race-track.tsx
@@ -0,0 +1,15 @@
+
+import React from "react";
+import Track from "./Track";
+
+function RaceTrack() {
+  return (
+    <div>
+      <Track>
+        <p>car</p>
+      </Track>
+    </div>
+  );
+}
+
+export default RaceTrack;
